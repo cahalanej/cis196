1b30b6ee3eaf5ea18bd6f2be223dbd4e3b08b2f2
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2013-10-29 09:00:33 -0400

    mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,14 +51,17 @@ Task::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {:host => "http\://vast-cove-8738.herokuapp.com"}
+  config.action_mailer.default_url_options = {:host => "vast-cove-8738.herokapp.com",
+        :only_path => true}
+
+config.action_mailer.delivery_method = :smtp 
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :user_name  => ENV['SENDGRID_USERNAME'],
     :password   => ENV['SENDGRID_PASSWORD'],
-    :domain         => "http\://vast-cove-8738.herokuapp.com"
+    :domain         => ENV['SENDGRID_DOMAIN']
   }
   # Enable threaded mode
   # config.threadsafe!
